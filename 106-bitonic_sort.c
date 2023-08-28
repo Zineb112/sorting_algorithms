6375cee3db0016e744392500c11abbfff92cc717
kawakibb <kawakibk7@gmail.com>
2023-08-28 21:31:29 +0000

    sorting_algorithms

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 void swap_nbrs(int *a, int *b);
-void bitonic_mrg(int *array, size_t size, size_t start, size_t seque,char flw);
+void bitonic_mrg(int *array, size_t size, size_t start, size_t seque,
+		char flw);
 void bitonic_seque(int *array, size_t size, size_t start, size_t seque, char flw);
 void bitonic_sort(int *array, size_t size);
 
